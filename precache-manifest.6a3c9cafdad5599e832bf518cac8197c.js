569fff1719b527e33cd6fbd86cd2a42cf1abe399
Gabe Perez <gabeperez@Gabes-MBP.fios-router.home>
2020-09-20 02:04:52 -0400

    Updates

---
+++ precache-manifest.6a3c9cafdad5599e832bf518cac8197c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9a9e9ebe84220b7740bb917a5af309",
+    "url": "/gabrielperez/index.html"
+  },
+  {
+    "revision": "9d5165ed4759846057c2",
+    "url": "/gabrielperez/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69122720512492a8ccd3",
+    "url": "/gabrielperez/static/js/2.e509c257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gabrielperez/static/js/2.e509c257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5165ed4759846057c2",
+    "url": "/gabrielperez/static/js/main.a0ca26de.chunk.js"
+  },
+  {
+    "revision": "3c0300271338c20fcd14",
+    "url": "/gabrielperez/static/js/runtime-main.1bcef83e.js"
+  }
+]);
